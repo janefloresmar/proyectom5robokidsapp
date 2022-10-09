2f0d89cce6d185302120b09a45c1a3d621484e44
janefloresmar <carolineflores75@hotmail.com>
2022-10-09 01:04:06 -0400

    proy5FS1

---
+++ cursos/cursosrobot/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.pytho
+from .models import Curso
+
+# Register your models here.
+
+admin.site.register(Curso)
---
+++ proyJFM5fullstack/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for proyJFM5fullstack project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyJFM5fullstack.settings')
+
+application = get_wsgi_application()
